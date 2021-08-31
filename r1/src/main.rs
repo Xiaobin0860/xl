1a1f2f9a403d5dcee7c2f08ca395a246a3214a3b
xl000 <l_xb@foxmail.com>
2021-08-31 13:09:57 +0800

    fib

--- r1/src/main.rs
+++ r1/src/main.rs
@@ -29,3 +29,63 @@ fn main() {
     fs::write(output, md.as_bytes()).unwrap();
     println!("Converted markdown has been saved to {}", output);
 }
+
+pub fn fib_loop(n: u8) -> i32 {
+    let mut a = 1;
+    let mut b = 1;
+    let mut i = 2;
+    loop {
+        if i >= n {
+            break;
+        }
+
+        let c = a + b;
+        a = b;
+        b = c;
+        i += 1;
+    }
+
+    b
+}
+
+pub fn fib_while(n: u8) -> i32 {
+    let (mut a, mut b, mut i) = (1, 1, 2);
+    while i < n {
+        let c = a + b;
+        a = b;
+        b = c;
+        i += 1;
+    }
+    b
+}
+
+pub fn fib_for(n: u8) -> i32 {
+    let (mut a, mut b) = (1, 1);
+    for _ in 2..n {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(1, fib_loop(1));
+        assert_eq!(1, fib_loop(2));
+        assert_eq!(2, fib_loop(3));
+        assert_eq!(55, fib_loop(10));
+        assert_eq!(1, fib_while(1));
+        assert_eq!(1, fib_while(2));
+        assert_eq!(2, fib_while(3));
+        assert_eq!(55, fib_while(10));
+        assert_eq!(1, fib_for(1));
+        assert_eq!(1, fib_for(2));
+        assert_eq!(2, fib_for(3));
+        assert_eq!(55, fib_for(10));
+    }
+}
