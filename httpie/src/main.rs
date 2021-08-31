dcb4a8c40ede50fc13f02609496edb70369ce562
xl000 <l_xb@foxmail.com>
2021-08-31 19:25:56 +0800

    httpie clap check args

--- httpie/src/main.rs
+++ httpie/src/main.rs
@@ -1,4 +1,8 @@
+use std::str::FromStr;
+
+use anyhow::{anyhow, Result};
 use clap::{AppSettings, Clap};
+use reqwest::Url;
 
 #[derive(Clap, Debug)]
 struct Opts {
@@ -14,13 +18,44 @@ enum SubCommand {
 
 #[derive(Clap, Debug)]
 struct Get {
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
+}
+
+fn parse_url(s: &str) -> Result<String> {
+    let _: Url = s.parse()?;
+    Ok(s.into())
 }
 
 #[derive(Clap, Debug)]
 struct Post {
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
-    body: Vec<String>,
+    #[clap(parse(try_from_str = parse_kv))]
+    body: Vec<KV>,
+}
+
+#[derive(Debug)]
+struct KV {
+    k: String,
+    v: String,
+}
+
+impl FromStr for KV {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split("=");
+        let err = || anyhow!(format!("Failed to parse {}", s));
+        Ok(Self {
+            k: (split.next().ok_or_else(err)?).to_string(),
+            v: (split.next().ok_or_else(err)?).to_string(),
+        })
+    }
+}
+
+fn parse_kv(s: &str) -> Result<KV> {
+    Ok(s.parse()?)
 }
 
 fn main() {
