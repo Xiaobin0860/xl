eecc42bdf16d66db57a47d1ab086f164301abc84
xl000 <l_xb@foxmail.com>
2021-09-16 18:07:43 +0800

    strtok与c函数行为一致

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -1,8 +1,22 @@
-fn strtok<'a>(s: &mut &'a str, delimiter: char) -> &'a str {
-    if let Some(i) = s.find(delimiter) {
-        let prefix = &s[..i];
-        *s = &s[i + delimiter.len_utf8()..];
-        prefix
+fn strtok<'a, 'b>(s: &mut &'a str, delimiters: &'b str) -> &'a str {
+    let mut idx = usize::MAX;
+    let mut delimiter = ' ';
+    for c in delimiters.chars() {
+        if let Some(i) = s.find(c) {
+            if i < idx {
+                idx = i;
+                delimiter = c;
+            }
+        }
+    }
+    if idx == 0 {
+        *s = &s[delimiter.len_utf8()..];
+        return strtok(s, delimiters);
+    }
+    if idx < s.len() {
+        let prefix = &s[..idx];
+        *s = &s[idx + delimiter.len_utf8()..];
+        return prefix;
     } else {
         let prefix = *s;
         *s = "";
@@ -13,17 +27,17 @@ fn strtok<'a>(s: &mut &'a str, delimiter: char) -> &'a str {
 fn main() {
     let s = "hello world".to_owned();
     let mut s1 = s.as_str();
-    let hello = strtok(&mut s1, ' ');
+    let hello = strtok(&mut s1, " ");
     println!("hello is: \"{}\", s1: \"{}\", s: \"{}\"", hello, s1, s);
 
     let mut s1 = s.as_str();
-    let hello = strtok(&mut s1, ',');
+    let hello = strtok(&mut s1, ",");
     println!("hello is: \"{}\", s1: \"{}\", s: \"{}\"", hello, s1, s);
 
-    let s = "This a sample string".to_owned();
+    let s = "-This, a sample string.".to_owned();
     let mut s1 = s.as_str();
     while !s1.is_empty() {
-        let word = strtok(&mut s1, ' ');
+        let word = strtok(&mut s1, " ,.-");
         println!("\"{}\"", word);
     }
 }
