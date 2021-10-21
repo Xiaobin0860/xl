cc02acc23bf369be686057d6e37cb0f8006e83e4
xl000 <l_xb@foxmail.com>
2021-10-21 14:11:34 +0800

    regex

--- rg/src/error.rs
+++ rg/src/error.rs
@@ -2,6 +2,8 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum GrepError {
+    #[error("Regex error")]
+    RegexError(#[from] regex::Error),
     #[error("I/O error")]
     IoError(#[from] std::io::Error),
 }
