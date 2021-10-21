578123d35affa5e6790d48cafe4c1e47915999b0
xl000 <l_xb@foxmail.com>
2021-10-21 11:22:47 +0800

    sample find

--- rg/src/main.rs
+++ rg/src/main.rs
@@ -1,4 +1,5 @@
 use clap::{App, Arg};
+use colored::*;
 use rg::GrepError;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -20,15 +21,27 @@ fn main() -> Result<(), GrepError> {
         .get_matches();
     println!("{:?}", matches);
     let is_regex = matches.is_present("regex");
-    let pattern = matches.value_of("pattern").unwrap();
+    let pattern: String = matches.value_of("pattern").unwrap().to_string();
     let file = matches.value_of("file").unwrap_or_default();
     println!("regex={}, pattern={}, file={}", is_regex, pattern, file);
 
     let f = File::open(file)?;
     let r = BufReader::new(f);
-    r.lines()
-        .enumerate()
-        .for_each(|(no, line)| println!("{}: {}", no, line.unwrap()));
+    r.lines().enumerate().for_each(|(no, line)| match line {
+        Ok(l) => {
+            if let Some(s) = l.find(pattern.as_str()) {
+                let end = s + pattern.len();
+                println!(
+                    "{:0>4}: {}{}{}",
+                    no.to_string().blue(),
+                    &l[..s],
+                    &l[s..end].red(),
+                    &l[end..],
+                )
+            }
+        }
+        Err(e) => println!("{}: {:?}", no.to_string().blue(), e),
+    });
 
     Ok(())
 }
